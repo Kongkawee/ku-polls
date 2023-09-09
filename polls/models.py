e968e09450b31f804d722d9a85dd661f92aaed50
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2023-09-09 20:21:47 +0700

    add end_date attribute

--- polls/models.py
+++ polls/models.py
@@ -15,6 +15,7 @@ class Question(models.Model):
     """
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    end_date = models.DateTimeField('date suppressed')
 
     def __str__(self):
         return self.question_text
