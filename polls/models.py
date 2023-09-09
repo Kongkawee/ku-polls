d65561393b4056a105118c397e8ff5e9f792760a
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2023-09-09 22:39:58 +0700

    Add default date for polls and Add 2 methods

--- polls/models.py
+++ polls/models.py
@@ -14,8 +14,8 @@ class Question(models.Model):
         was_published_recently: Returns a boolean indicating whether this poll question is currently published or not.
     """
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    end_date = models.DateTimeField('date suppressed')
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    end_date = models.DateTimeField('date suppressed', null=True)
 
     def __str__(self):
         return self.question_text
@@ -32,6 +32,14 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_published(self):
+        now = timezone.now()
+        return now >= self.pub_date
+
+    def can_vote(self):
+        now = timezone.now()
+        return self.is_published() and now < self.end_date
 
 
 class Choice(models.Model):
