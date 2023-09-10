aa16e70173a48b8e2d4918cba58be934f21fc643
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2023-09-10 10:23:42 +0700

    Add docstring and checked with flake8

--- polls/models.py
+++ polls/models.py
@@ -8,21 +8,15 @@ from django.contrib import admin
 class Question(models.Model):
     """
     A template model representing questions.
-
-    Methods:
-        __str__: Returns the text of the question model.
-        was_published_recently: Returns a boolean indicating whether this poll question is currently published or not.
     """
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published', default=timezone.now)
-    end_date = models.DateTimeField('date suppressed', null=True, default=None, blank=True)
+    end_date = models.DateTimeField('date suppressed', null=True,
+                                    default=None, blank=True)
 
     def __str__(self):
+        """Return the text of the question"""
         return self.question_text
-
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     @admin.display(
         boolean=True,
@@ -30,14 +24,17 @@ class Question(models.Model):
         description='Published recently?',
     )
     def was_published_recently(self):
+        """Return True if the poll was published recently."""
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def is_published(self):
+        """Return True if the poll is published."""
         now = timezone.now()
         return now >= self.pub_date
 
     def can_vote(self):
+        """Return True if the poll is in the voting period."""
         now = timezone.now()
         if self.end_date is None:
             return self.is_published()
@@ -47,13 +44,11 @@ class Question(models.Model):
 class Choice(models.Model):
     """
     A template model representing choices.
-
-    Returns:
-        __str__: Returns the text of the choices model.
     """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
+        """Return text of the choice"""
         return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -9,6 +9,9 @@ from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
+    """
+    View for displaying a list of the published questions.
+    """
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
@@ -23,6 +26,10 @@ class IndexView(generic.ListView):
 
 
 class DetailView(generic.DetailView):
+    """
+    View for display detail of each poll question
+    including a list of choices.
+    """
     model = Question
     template_name = 'polls/detail.html'
 
@@ -33,6 +40,10 @@ class DetailView(generic.DetailView):
         return Question.objects.filter(pub_date__lte=timezone.now())
 
     def get(self, request, *args, **kwargs):
+        """
+        Receive the request from the user and catch
+        the direct-access attempt to the unpublished polls.
+        """
         try:
             self.object = self.get_object()
             if not self.object.can_vote():
@@ -47,11 +58,15 @@ class DetailView(generic.DetailView):
 
 
 class ResultsView(generic.DetailView):
+    """View for display result of the poll"""
     model = Question
     template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
+    """
+    View for handling user votes for a specific poll question.
+    """
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
@@ -64,4 +79,5 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results',
+                                            args=(question.id,)))
