d6a0aaa0fb468491cae2bdcacc2ed9c0c920b0c0
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2023-09-17 14:15:46 +0700

    Create vote confirmation message.

--- polls/views.py
+++ polls/views.py
@@ -91,7 +91,9 @@ def vote(request, question_id):
         vote = Vote(user=this_user, choice=selected_choice)
 
     vote.save()
-    # TODO: Use messages to display a confirmation on the results page.
+
+    messages.success(request, f"Your vote for '{vote.choice}' has been saved.")
+
     return HttpResponseRedirect(
         reverse('polls:results', args=(question_id,))
     )
